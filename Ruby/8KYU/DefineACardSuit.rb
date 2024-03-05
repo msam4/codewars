eef36cd69a7f9069ee77a8c7df6fef0c301bc382
Michael Sam <sam.michael.h@gmail.com>
2024-03-05 20:26:49 +0900

    Created Ruby folder. Created 8KYU folder. Add define a card suit, exclamation mark series 11, and sentence smash question and my solution

---
+++ Ruby/8KYU/DefineACardSuit.rb
@@ -0,0 +1,35 @@
+# You get any card as an argument. Your task is to return the suit of this card (in lowercase).
+
+# Our deck (is preloaded):
+
+# DECK = ['2S','3S','4S','5S','6S','7S','8S','9S','10S','JS','QS','KS','AS',
+#         '2D','3D','4D','5D','6D','7D','8D','9D','10D','JD','QD','KD','AD',
+#         '2H','3H','4H','5H','6H','7H','8H','9H','10H','JH','QH','KH','AH',
+#         '2C','3C','4C','5C','6C','7C','8C','9C','10C','JC','QC','KC','AC']
+# ('3C') -> return 'clubs'
+# ('3D') -> return 'diamonds'
+# ('3H') -> return 'hearts'
+# ('3S') -> return 'spades'
+
+#MY SOLUTION
+def define_suit(card)
+  if card.include?('C')
+    return 'clubs'
+  elsif card.include?('D')
+    return 'diamonds'
+  elsif card.include?('H')
+    return 'hearts'
+  elsif card.include?('S')
+    return 'spades'
+  end
+end
+
+#Other SOLUTION
+def define_suit(card)
+  case card[-1]
+    when "C" then "clubs"
+    when "S" then "spades"
+    when "D" then "diamonds"
+    when "H" then "hearts"
+  end
+end
