224d74985cf816ad8994feefcb70d5fd5f54bd9e
Michael Sam <sam.michael.h@gmail.com>
2024-03-06 14:36:10 +0900

    Add invert value and reversed string question and solution

---
+++ Ruby/8KYU/ReversedStrings.rb
@@ -0,0 +1,10 @@
+# DESCRIPTION:
+# Complete the solution so that it reverses the string passed into it.
+
+# 'world'  =>  'dlrow'
+# 'word'   =>  'drow'
+
+# MY SOLUTION
+def solution(str)
+  str.reverse()
+end
