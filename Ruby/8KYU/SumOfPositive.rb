f945cbee938f86fd82462c5cdd8521507538a7a6
Michael Sam <sam.michael.h@gmail.com>
2024-03-08 10:36:22 +0900

    Add sum of positive and convert a string to a number question and solution for ruby

---
+++ Ruby/8KYU/SumOfPositive.rb
@@ -0,0 +1,11 @@
+# DESCRIPTION:
+# You get an array of numbers, return the sum of all of the positives ones.
+
+# Example [1,-4,7,12] => 1 + 7 + 12 = 20
+
+# Note: if there is nothing to sum, the sum is default to 0.
+
+# MY SOLUTION
+def positive_sum(arr)
+  arr.select{ |num| num > 0 }.sum
+end
