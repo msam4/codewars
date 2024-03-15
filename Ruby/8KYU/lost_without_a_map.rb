53f18e717988488ba0a89539caea65b43b64fc17
Michael Sam <sam.michael.h@gmail.com>
2024-03-15 13:58:16 +0900

    Add lost without a map question and solution for ruby

---
+++ Ruby/8KYU/lost_without_a_map.rb
@@ -0,0 +1,11 @@
+# DESCRIPTION:
+# Given an array of integers, return a new array with each value doubled.
+
+# For example:
+
+# [1, 2, 3] --> [2, 4, 6]
+
+# MY SOLUTION
+def maps(x)
+  x.each.map{ |i| i * 2}
+end
