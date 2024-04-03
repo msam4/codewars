7daa704ce6fa8884ea33e6afa9574395786c9a79
Michael Sam <sam.michael.h@gmail.com>
2024-04-03 15:14:00 +0900

    Add grasshopper personalized message problem and solution for ruby

---
+++ Ruby/8KYU/grasshopper_personalized_message.rb
@@ -0,0 +1,17 @@
+# DESCRIPTION:
+# Create a function that gives a personalized greeting. This function takes two parameters: name and owner.
+
+# Use conditionals to return the proper message:
+
+# case	return
+# name equals owner	'Hello boss'
+# otherwise	'Hello guest'
+
+# MY SOLUTION
+def greet(name,owner)
+  if name == owner
+    "Hello boss"
+  else
+    "Hello guest"
+  end
+end
